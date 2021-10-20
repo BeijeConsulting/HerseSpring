f85b5f37de2a4fd034cbeb235184078ae934334f
ivfly <iv0fly@gmail.com>
2021-10-20 11:25:35 +0200

    @Service + @Query

---
+++ src/main/java/it/beije/herse/repository/OrderRepository.java
@@ -0,0 +1,23 @@
+package it.beije.herse.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import it.beije.herse.entity.Order;
+
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+	
+	public List<Order> findByDateTimeGreaterThan(LocalDateTime dateTime);
+	
+//	@Query(nativeQuery = true, value = "SELECT * FROM `order` WHERE creation_datetime > :dateTime")
+	@Query(value = "SELECT o FROM Order as o WHERE o.dateTime > :dateTime")
+	public List<Order> searchByDateTimeGreaterThan(@Param("dateTime") LocalDateTime dateTime);
+	
+}
