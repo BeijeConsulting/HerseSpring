de8a756bc9e1f22f5895f511b4d417626dd9984f
nmanfredi3 <nicola.manfredi.210796@gmail.com>
2021-10-25 18:00:56 +0200

    Some stuff

--- src/main/java/it/beije/herse/my/restcontroller/MyOrderItemsRestController.java
+++ src/main/java/it/beije/herse/my/restcontroller/MyOrderItemsRestController.java
@@ -1,6 +1,8 @@
 package it.beije.herse.my.restcontroller;
 
+import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder.In;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,52 +14,69 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.beije.herse.bean.OrderItems;
+import it.beije.herse.bean.Orders;
 import it.beije.herse.my.service.MyOrderItemsService;
 
 @RestController
 @RequestMapping("api")
 public class MyOrderItemsRestController {
-	
+
 	@Autowired
 	private MyOrderItemsService myOrderItemsService;
-	
+
 	@GetMapping("/orderItems/details/{id}")
-	public OrderItems getOrderItems(@PathVariable("id") Integer id){
+	public OrderItems getOrderItems(@PathVariable("id") Integer id) {
 		OrderItems oi = myOrderItemsService.findById(id);
-		
+
 		return oi;
 	}
-	
+
+	@GetMapping("/orderItems/detail")
+	public List<OrderItems> getOrder() {
+		List<OrderItems> orderItems = myOrderItemsService.findAll();
+		if (orderItems.size() == 0) {
+			System.out.println("non ci sono ordini");
+			return null;
+		}
+		return orderItems;
+	}
+
 	@PostMapping("/orderItems/insert")
 	public OrderItems insert(@RequestBody OrderItems orderItem) {
 		System.out.println("OrderItem: " + orderItem);
-		
+
 		myOrderItemsService.save(orderItem);
-		
-		return orderItem;	
+
+		return orderItem;
 	}
-	
+
 	@PutMapping("/orderItems/update/{id}")
-	public OrderItems update(@PathVariable("id") Integer id,@RequestBody OrderItems orderItem) {
-		if(orderItem.getId() != null && orderItem.getId() != 0) {
+	public OrderItems update(@PathVariable("id") Integer id, @RequestBody OrderItems orderItem) {
+		if (orderItem.getId() != null && orderItem.getId() != 0) {
 			throw new RuntimeException("id non corrispondente");
 		}
 		OrderItems oi = myOrderItemsService.findById(id);
 		myOrderItemsService.updateOrder(oi, orderItem);
 		myOrderItemsService.save(oi);
-		
+
 		return oi;
 	}
-	
+
 	@PutMapping("/orderItems/delete/{id}")
 	public void delete(@PathVariable("id") Integer id) {
 		OrderItems orderItems = myOrderItemsService.findById(id);
 		String response;
-		if(orderItems != null) {
+		if (orderItems != null) {
 			response = myOrderItemsService.deleteOrder(orderItems) ? "deleted" : "not deleted";
 			System.out.println(response);
-		}else {
+		} else {
 			System.out.println("OrderItem not found");
 		}
+	}
+	
+	@GetMapping("/orderItems/detail/order/{orderId}")
+	public List<OrderItems> getFromUsers(@PathVariable("orderId") Integer orderId){
+		List<OrderItems> orderItems = myOrderItemsService.findByOrderId(orderId);
+		return orderItems;
 	}
 }
--- src/main/java/it/beije/herse/my/service/MyOrderItemsService.java
+++ src/main/java/it/beije/herse/my/service/MyOrderItemsService.java
@@ -1,5 +1,6 @@
 package it.beije.herse.my.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
@@ -7,33 +8,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.beije.herse.bean.OrderItems;
+import it.beije.herse.bean.Orders;
 import it.beije.herse.my.repository.MyOrderItemsRepository;
 
 @Service
 public class MyOrderItemsService {
-	
+
 	@Autowired
 	private MyOrderItemsRepository myOrderItemsRepository;
 
 	public OrderItems findById(Integer id) {
 		Optional<OrderItems> oi = myOrderItemsRepository.findById(id);
-		
-		return  oi.isPresent() ? oi.get() : null;	
+
+		return oi.isPresent() ? oi.get() : null;
 	}
 
 	public void save(OrderItems orderItem) {
-			myOrderItemsRepository.save(orderItem);
+		myOrderItemsRepository.save(orderItem);
 	}
 
 	public OrderItems updateOrder(OrderItems oi, OrderItems orderItem) {
 		BeanUtils.copyProperties(orderItem, oi, new String[] { "id" });
-		return oi;		
+		return oi;
 	}
 
 	public boolean deleteOrder(OrderItems orderItems) {
 		myOrderItemsRepository.delete(orderItems);
 		return true;
 	}
-	
-	
+
+	public List<OrderItems> findAll() {
+		List<OrderItems> orderItems = myOrderItemsRepository.findAll();
+		return orderItems;
+	}
+
+	public List<OrderItems> findByOrderId(Integer orderId) {
+		List<OrderItems> orderItems = myOrderItemsRepository.findByOrderId(orderId);
+		return orderItems;
+	}
+
 }
